e95ec8e4794555158b5c155ebf50cb735f025857
ajmastaflex <adrien.moevus@gmail.com>
2020-10-13 21:53:37 -0400

    added support for https

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
+import HttpsRedirect from 'react-https-redirect';
 ReactDOM.render(
+ 
   <React.StrictMode>
+   <HttpsRedirect>
     <App />
+    </HttpsRedirect>
   </React.StrictMode>,
+
   document.getElementById('root')
 );
 
